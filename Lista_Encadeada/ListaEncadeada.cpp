4143f2d223a0d6291885234cd49f3e8cf6d2f494
liberato-utfpr <liberato@utfpr.edu.br>
2023-10-27 14:46:44 -0300

    aula 25/10/23

--- Lista_Encadeada/ListaEncadeada.cpp
+++ Lista_Encadeada/ListaEncadeada.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+
+
+
 ListaEncadeada::ListaEncadeada(){
     this->qtd = 0;
     this->primeiro = nullptr;
@@ -21,7 +24,77 @@ bool ListaEncadeada::anexar(int elemento){
     }
     this->ultimo = novo;
     this->qtd++;
+    return true;
 }
+
+
+int ListaEncadeada::removerPorElemento(int elemento){
+    if (this->qtd == 1){
+        
+        No* noRemovido = this->primeiro;
+        this->primeiro = nullptr;
+        this->ultimo = nullptr;
+        delete noRemovido;
+        this->qtd--;
+        return 0;
+
+    }else if (this->primeiro->dado == elemento){ // Remoção na primeira posição
+        
+        No* noRemovido = this->primeiro;
+        this->primeiro = this->primeiro->prox;
+        
+        delete noRemovido;
+        this->qtd--;
+        return 0;
+        
+    } else if (this->ultimo->dado == elemento){ // remoção da última posição
+        No* aux = this->primeiro;
+        
+        // Leva o aux até o último elemento
+        for (int i=0; i < this->qtd-2; i++)
+            aux = aux->prox;
+        
+        // atualiza o this->ultimo com o endereço do penúltimo elemento
+        this->ultimo = aux;
+        // Atualiza o aux para o elemento a ser removido
+        aux = aux->prox;
+
+        this->ultimo->prox = nullptr;
+        // Levando o aux para o último elemento
+    
+        delete aux;
+        this->qtd--;
+        return this->qtd;
+
+
+        
+
+        
+    } else {        
+        No* aux = this->primeiro;
+        int posicao = 1;
+
+        while (aux->prox != nullptr){
+            No* sucessor = aux->prox;
+
+            if (sucessor->dado == elemento){
+                aux->prox = sucessor->prox;
+                // aux->prox = aux->prox->prox;
+                delete sucessor;
+                this->qtd--;
+                return posicao;
+            }
+            aux = aux->prox;
+            posicao++;
+        }
+        
+        return -1;
+    }
+
+
+}
+
+
 
 void ListaEncadeada::imprimir(){
     cout << "[";
--- Lista_Encadeada/main.cpp
+++ Lista_Encadeada/main.cpp
@@ -5,9 +5,29 @@ using namespace std;
 
 int main(){
     ListaEncadeada* lista = new ListaEncadeada();
-    lista->anexar(50);
-    lista->anexar(60);
-    lista->anexar(70);
-    lista->imprimir(); // [50,60,70]
+    lista->anexar(10);
+    lista->anexar(20);
+    lista->anexar(30);
+    lista->anexar(40);
+    lista->imprimir();              // [10,20,30,40]
+
+   
+
+
+    
+    // cout << lista->removerPorElemento(100) << endl;
+
+    lista->removerPorElemento(30);
+    lista->imprimir();              // [10,20,40]
+    lista->removerPorElemento(10);
+    lista->imprimir();              // [20,40]
+    lista->removerPorElemento(40);
+    lista->imprimir();              // [20]
+    lista->removerPorElemento(20);
+    lista->imprimir();              // []
+
+    
+
+
 
 }
